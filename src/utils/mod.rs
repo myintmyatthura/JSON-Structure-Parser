027e7b9297f87922dc27ed5834edfb2cf374a122
myintmyatthura <myintmyatthura.jr@gmail.com>
2024-06-23 20:54:07 +0800

    first commit

---
+++ src/utils/mod.rs
@@ -0,0 +1,3 @@
+pub mod args;
+pub mod json;
+pub mod read_text;
---
+++ src/utils/read_text.rs
@@ -0,0 +1,30 @@
+use std::fs;
+use std::path::PathBuf;
+
+pub fn read_text() -> String {
+    // Create a PathBuf for the current file
+    let current_file = PathBuf::from(file!());
+    // Navigate to the parent directory of the current file
+    let parent_dir = current_file
+        .parent()
+        .expect("Failed to get parent directory\n");
+
+        
+    // Create a relative path to file1.txt
+    let file1_path = parent_dir.join("../input/file1.txt");
+
+    if file1_path.exists() {
+        println!("This exists, the path is correct!\n");
+    } else {
+        println!("This doesn't exist, please check path.\n");
+        return "".to_string();
+    }
+
+    match fs::read_to_string(file1_path) {
+        Ok(contents) => contents,
+        Err(err) => {
+            eprintln!("Error reading file: {:?}", err);
+            "".to_string()
+        }
+    }
+}
