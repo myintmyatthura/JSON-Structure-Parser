7f83cc7bd2c60d4bf916605dd6125ad5e06da5b1
myintmyatthura <myintmyatthura.jr@gmail.com>
2024-06-25 21:39:42 +0800

    Added a JSON parser for single units, need to modify to run for an array

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,13 @@ pub mod constants;
 pub mod utils;
 
 fn main() {
-    let parsed_string = utils::read_text::read_text();
-    let collections = utils::json::ser(parsed_string).unwrap();
-    println!("{:#?}", collections);
+    // let parsed_string = utils::read_text::read_text();
+    // let collections = utils::json::ser(parsed_string).unwrap();
+    // // println!("{:#?}", collections);
+
+    // let collection_iter = collections.iter();
+
+    // utils::json::p
+
     // implement string-matching algorithm..
 }
--- src/utils/json.rs
+++ src/utils/json.rs
@@ -1,7 +1,6 @@
-
-
 use serde::{Deserialize, Serialize};
 use serde_json::from_str;
+use std::collections::HashMap;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct JsonDict {
@@ -12,6 +11,37 @@ pub struct JsonDict {
     pub fields: Vec<String>,
 }
 
+impl JsonDict {
+    // Method to modify field1 and return the entire struct
+    fn parse_description(mut self, data_dict: &HashMap<&str, &str>) -> Self {
+        let parts_name: Vec<&str> = self.name.split('_').collect();
+
+        let mut new_parts: Vec<String> = Vec::new();
+        for part in &parts_name {
+            if let Some(&value) = data_dict.get(part) {
+                new_parts.push(value.to_string());
+            } else {
+                new_parts.push(part.to_string());
+            }
+        }
+
+        let final_value = new_parts.join(" ");
+        self.description = Some(final_value);
+
+        self
+    }
+}
+
+pub fn create_json() -> JsonDict {
+    return JsonDict {
+        name: "SS_MM".to_string(),
+        mode: "NULLABLE".to_string(),
+        r#type: "STRING".to_string(),
+        description: None,
+        fields: Vec::from([]),
+    };
+}
+
 pub fn ser(input: String) -> Option<Vec<JsonDict>> {
     //Deserialize the JSON string into your struct object
     let deserialized_obj: Result<Vec<JsonDict>, _> = from_str(&input);
@@ -19,19 +49,18 @@ pub fn ser(input: String) -> Option<Vec<JsonDict>> {
     match deserialized_obj {
         Ok(obj) => {
             return Some(obj);
-            // Modify the struct fields as needed
-            // For example:
-            // obj.field1 = "New Value".to_string();
-
-            // Serialize the modified object back to JSON
-            // let serialized_json = to_string_pretty(&obj).unwrap();
-            // println!("Serialized JSON: {}", serialized_json);
         }
         Err(err) => {
             eprintln!("Error deserializing JSON: {:?}", err);
             return None;
         }
     }
+}
+
+pub fn parse_string(input: JsonDict, dict: &HashMap<&str, &str>) -> JsonDict {
+    let output = input.parse_description(dict);
+
+    return output;
 }
 
 pub fn deser() {
