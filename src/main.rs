d8711e6a32dc5c4c81c0f6f688f0bdc99111a368
myintmyatthura <myintmyatthura.jr@gmail.com>
2024-06-26 23:28:23 +0800

    additional documentation for main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,15 @@ pub mod constants;
 pub mod utils;
 
 fn main() {
-    // let parsed_string = utils::read_text::read_text();
-    // let modified_contents = parsed_string.replace("null", "\"null\"");
-    // println!("{:#?}", modified_contents);
-    // let collections = utils::json::ser(modified_contents).unwrap();
-    // println!("{:#?}", collections);
+    let parsed_string = utils::read_text::read_text();
+    let modified_contents = parsed_string.replace("null", "\"null\"");
+    println!("{:#?}", modified_contents);
+    let collections = utils::json::ser(modified_contents).unwrap();
+    println!("{:#?}", collections);
 
-    let new_json_object = utils::json::create_json();
-    let my_dict = constants::dictionary::dictionary();
-    let _my_output = utils::json::parse_string(new_json_object, &my_dict);
+    // let new_json_object = utils::json::create_json();
+    // let my_dict = constants::dictionary::dictionary();
+    // let _my_output = utils::json::parse_string(new_json_object, &my_dict);
 
     // implement string-matching algorithm..
 }
